968ae4aaf17c3c4d3df93988e7c06c0285d0920a
stakezone <41361219+stakezone@users.noreply.github.com>
2020-04-15 06:30:52 -0400

    Update oraclemonitor.sh

--- oraclemonitor.sh
+++ oraclemonitor.sh
@@ -6,7 +6,7 @@
 config=""              # config.toml file for node, eg. /home/user/.terrad/config/config.toml
 terracli=""            # the terracli binary, eg. /home/user/go/bin/terracli
 validatorpubkey=""     # terravaloper* pubkey, is used to find the feeder address
-nmissedoraclevotes=20  # checks back the missed oracle votes n blocks from blockheight, a value higher than 100 is not supported by terrad
+nmissedoraclevotes=100  # checks back the missed oracle votes n blocks from blockheight, a value higher than 100 is not supported by terrad
 logname=""             # a custom log file name can be chosen, if left empty default is oraclemonitor-<username>.log
 logpath="$(pwd)"       # the directory where the log file is stored, for customization insert path like: /my/path
 logsize=200            # the max number of lines after that the log will be trimmed to reduce its size
